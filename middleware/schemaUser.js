3f22131df912a7e3e97fb2d7a0e3a810e54505d8
sofiagaona <sofia.herrera.ga@gmail.com>
2021-11-11 11:35:03 -0600

    validar que use registre un nuevo usario sin que exist ya el email, usar los errores desde el middlleware

--- middleware/schemaUser.js
+++ middleware/schemaUser.js
@@ -1,10 +1,19 @@
+const User = require('../model/model_user');
+
 module.exports = (schema) => {
   const result = async (req, res, next) => {
-    try {
-      await schema.validateAsync({ email: req.body.email, password: req.body.password });
-      next();
-    } catch (error) {
-      res.send(error.message);
+    const userByEmail = await User.find({ email: req.body.email });
+    console.info(userByEmail);
+    if (userByEmail.length > 0) {
+      res.status(400).json({ error: 'el usario ya existe' });
+    } else {
+      try {
+        await schema.validateAsync({ email: req.body.email, password: req.body.password });
+        next();
+      } catch (error) {
+        next(error);
+        // res.send(error.message);
+      }
     }
   };
   return result;
--- model/model_user.js
+++ model/model_user.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   roles: { type: Object, required: true },
-  admin: { type: Boolean, required: true },
+  // admin: { type: Boolean, required: true },
   estado: { type: Boolean, default: true },
 });
 module.exports = mongoose.model('User', userSchema);
